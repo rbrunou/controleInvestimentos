455a53813756552760d8f59f50d25e2e5b7f3a0d
rbrunou <renatobrunosilva17@gmail.com>
2022-03-03 22:06:00 -0300

    Integrando node com python

--- teste.py
+++ teste.py
@@ -1,19 +1,5 @@
-import cgi
-import cgitb
-cgitb.enable()
-
-input_data=cgi.FieldStorage()
-num1=0
-num2=0
-
-print ('Content-Type:text/html')
-print()
-print('<h1>Resultado da Soma</h1>')
-try:
-    num1=int(input_data["num1"].value)
-    num2=int(input_data["num2"].value)
-except:
-    print('<p>não deu certo<p>')
-
-sum = num1 + num2
-print('<p>{0} + {1} = {2}</p>'.format(num1, num2, sum))
+import random, time
+while True:
+    time.sleep(random.random() * 5)  # wait 0 to 5 seconds
+    temperature = (random.random() * 20) - 5  # -5 to 15
+    print(temperature, flush=True, end='')
